4b5237980cf69459644ff8c503fa9d62d41d6bb8
Vijay Pandurangan <vijayp@vijayp.ca>
2021-01-24 20:39:12 -0500

    move the html page and server to a different directory.

---
+++ simple-server/py/server.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import argparse
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+
+class MyServer(SimpleHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def _html(self, message):
+        """This just generates an HTML document that includes `message`
+        in the body. Override, or re-write this do do more interesting stuff.
+        """
+        content = f"<html><body><h1>{message}</h1></body></html>"
+        return content.encode("utf8")  # NOTE: must return a bytes object!
+
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        self._set_headers()
+        self.wfile.write("<html><body><h1>POST!</h1><pre>" + post_data + "</pre></body></html>")
+
+def run(server_class=HTTPServer, handler_class=MyServer, addr="localhost", port=8000):
+    server_address = (addr, port)
+    httpd = server_class(server_address, handler_class)
+
+    print(f"Starting httpd server on {addr}:{port}")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Run a simple HTTP server")
+    parser.add_argument(
+        "-l",
+        "--listen",
+        default="localhost",
+        help="Specify the IP address on which the server listens",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=8000,
+        help="Specify the port on which the server listens",
+    )
+    args = parser.parse_args()
+    run(addr=args.listen, port=args.port)
+
